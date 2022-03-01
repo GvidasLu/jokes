df90f65e0e1037c0b845106efe23b6db501a493e
GvidasLu <luspinis.gvidas@gmail.com>
2022-03-01 21:57:36 +0200

    save

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -6,12 +6,6 @@ const Gallery = () => {
     <div class="container-fluid">
         <div class="px-lg-5">
             <div class="row">
-        {/* {CardData.map((card)=>
-                    <Card
-                        key = {card.id}
-                        content = {card.joke}
-                    />
-                )} */}
             {JokesData.map((joke)=> {
                 if(joke.type == 'twopart')
                 return <Joke
